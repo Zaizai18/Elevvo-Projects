9e62f27e5a28ee32348a31d79592de6d61b7b07a
Zaizai18 <zainabshahzadi77@gmail.com>
2025-08-17 18:45:54 -0400

    Delete Sidebar directory

--- Sidebar/script.js
+++
@@ -1,6 +0,0 @@
-const sidebar = document.getElementById('sidebar1');
-        const toggleBtn = document.getElementById('toggleBtn');
-
-        toggleBtn.addEventListener('click', () => {
-            sidebar.classList.toggle('closed');
-        });
